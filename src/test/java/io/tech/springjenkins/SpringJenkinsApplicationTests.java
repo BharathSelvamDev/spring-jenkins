0c91643e5f881f3eb5cca1a47c15f39945cfc240
Bharath <bharath.tech.dev@gmail.com>
2022-11-20 12:03:17 +0530

    initial commit

---
+++ src/test/java/io/tech/springjenkins/SpringJenkinsApplicationTests.java
@@ -0,0 +1,15 @@
+package io.tech.springjenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class SpringJenkinsApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+        assertEquals(true , true);
+    }
+
+}
